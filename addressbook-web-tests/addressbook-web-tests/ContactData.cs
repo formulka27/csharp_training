6824e8da5832ae91f9e8dcead9f076c65e37dd82
formulka27 <kizhapkina@gmail.com>
2018-12-23 22:07:49 -0500

    Создание нового контакта .Объект контакт ,свойства Firstname и Lastname

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAaddressbookTests
+{
+    class ContactData
+    {
+        //объявляем поля
+        private string firstname;
+        private string lastname;
+        //значения по умолчанию можно и не указывать
+        private string middlename = "";
+        private string nickname = "";
+
+        //создаем конструктор для класса ContactData
+        public ContactData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+        //описываем свойства объекта , их два
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+    }
+
+}
